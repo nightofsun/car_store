0d68a0cbff0af46cd53a092a1e23c8d018f2767c
natthaphon <natthaphon@mfec.co.th>
2022-08-19 14:14:39 +0700

    add dependency

--- src/main/java/com/service/api/CarStoreApplication.java
+++ src/main/java/com/service/api/CarStoreApplication.java
@@ -1,13 +1,15 @@
-package com.service.api.CAR_STORE;
+package com.service.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.google.gson.Gson;
 
 @SpringBootApplication
 public class CarStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarStoreApplication.class, args);
+		Gson gson = new Gson();
 	}
 
 }
