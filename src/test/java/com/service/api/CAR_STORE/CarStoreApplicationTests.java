c45d8d692936b51f23b709f87309c4d2009fdea8
nightofsun <67412598+nightofsun@users.noreply.github.com>
2022-08-19 12:05:53 +0700

    Add files via upload

---
+++ src/test/java/com/service/api/CAR_STORE/CarStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.api.CAR_STORE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
