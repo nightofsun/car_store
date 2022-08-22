b5b2b92460f44637c16ab1bc170d68f1093407c7
natthaphon <natthaphon@mfec.co.th>
2022-08-22 15:16:28 +0700

    add BaseRequest

--- src/main/java/com/service/api/model/rest/BaseRequest.java
+++ src/main/java/com/service/api/model/rest/BaseRequest.java
@@ -1,4 +1,43 @@
 package com.service.api.model.rest;
 
+import java.util.Date;
+
 public class BaseRequest {
+    private String requestURI;
+    private Date requestDate;
+    private String bodyData;
+
+    public String getRequestURI() {
+        return requestURI;
+    }
+
+    public void setRequestURI(String requestURI) {
+        this.requestURI = requestURI;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public String getBodyData() {
+        return bodyData;
+    }
+
+    public void setBodyData(String bodyData) {
+        this.bodyData = bodyData;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("BaseRequest{requestUR=").append(requestURI);
+        sb.append(", requestDate").append(requestDate);
+        sb.append(", bodyData").append(bodyData);
+        sb.append("}");
+        return sb.toString();
+    }
 }
